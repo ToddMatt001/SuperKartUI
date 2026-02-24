4ba1479b2992ce43bc2a9f9ce4965b0658d18d17
ToddMatt001 <toddmattingly@gmail.com>
2026-02-23 21:46:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ import sklearn
 
 
 # ---- Google Drive model link (direct download)
-MODEL_URL = "https://huggingface.co/toddmattingly/SuperKart_RF/resolve/main/model.joblib"
+MODEL_URL = "https://huggingface.co/toddmattingly/SuperKart_RF/resolve/main/model_streamlit.joblib"
 MODEL_PATH = "model.joblib"
 
 
